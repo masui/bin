af2def6b942816c7aeb11c43ecb41c5a3f55e62e
Toshiyuki Masui <masui@pitecan.com>
2020-10-22 11:50:48 +0900

    ...

--- helpfeelbackup.rb
+++ helpfeelbackup.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/ruby
 
-DIR = "/Users/masui/Downloads"
+DIR = "#{ENV['HOME']}/Downloads"
+datadir = "#{ENV['HOME']}/HelpLine/omnibox/data"
 Dir.open(DIR){ |dir|
   dir.each { |f|
     if f =~ /helpfeel.*\.json/
       time = File.mtime("#{DIR}/#{f}").strftime("%Y%m%d%H%M%S")
-      puts "cat '#{DIR}/#{f}' | jq . > /Users/masui/HelpLine/omnibox/data/helpfeel-#{time}.json"
-      system "cat '#{DIR}/#{f}' | jq . > /Users/masui/HelpLine/omnibox/data/helpfeel-#{time}.json"
+      puts "cat '#{DIR}/#{f}' | jq . > #{datadir}/helpfeel-#{time}.json"
+      system "cat '#{DIR}/#{f}' | jq . > #{datadir}/helpfeel-#{time}.json"
       puts "/bin/rm '#{DIR}/#{f}'"
       system "/bin/rm '#{DIR}/#{f}'"
     end
